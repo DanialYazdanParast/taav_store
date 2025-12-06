09a5e0f6ee21b7c1f964c91159f855f661858cde
Danial YazdanParast <danyal.y0098@gmail.com>
2025-12-06 12:55:05 +0330

    feat: Add MainBuyer module and routing configuration

--- example/lib/src/pages/shared/models/nav_item_model.dart
+++ example/lib/src/pages/shared/models/nav_item_model.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class NavItemModel {
@@ -5,11 +6,35 @@ class NavItemModel {
   final IconData activeIcon;
   final String label;
   final bool isSpecial;
+  final int? badgeCount;
+  final bool showBadge;
 
-  NavItemModel({
+  const NavItemModel({
     required this.icon,
     required this.activeIcon,
     required this.label,
     this.isSpecial = false,
+    this.badgeCount,
+    this.showBadge = false,
   });
+
+  NavItemModel copyWith({
+    IconData? icon,
+    IconData? activeIcon,
+    String? label,
+    bool? isSpecial,
+    int? badgeCount,
+    bool? showBadge,
+  }) {
+    return NavItemModel(
+      icon: icon ?? this.icon,
+      activeIcon: activeIcon ?? this.activeIcon,
+      label: label ?? this.label,
+      isSpecial: isSpecial ?? this.isSpecial,
+      badgeCount: badgeCount ?? this.badgeCount,
+      showBadge: showBadge ?? this.showBadge,
+    );
+  }
+
+  bool get hasBadge => showBadge || (badgeCount != null && badgeCount! > 0);
 }
